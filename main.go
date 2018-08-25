0e35bad5e5d69f6d2e28019d171e59ab528e3eb7
petro <memozio@gmail.com>
2018-08-24 22:28:15 -0600

    new hello message

--- main.go
+++ main.go
@@ -2,27 +2,14 @@ package main
 
 import (
 	"io"
-	"io/ioutil"
-	"os"
-	"fmt"
 	"net/http"
 )
 
-func index(w http.ResponseWriter, r *http.Request) {
-	fmt.Printf("Handling %+v\n", r);
-	bs, err := ioutil.ReadFile("/content/index.html")
-
-	if err != nil {
-		fmt.Printf("Couldn't read index.html: %v", err);
-		os.Exit(1)
-	}
-
-	io.WriteString(w, string(bs[:]))
+func hello(w http.ResponseWriter, r *http.Request) {
+	io.WriteString(w, "Hello GCP CD!")
 }
 
 func main() {
-	http.HandleFunc("/", index)
-	port := ":8000"
-	fmt.Printf("Starting to service on port %s\n", port);
-	http.ListenAndServe(port, nil)
+	http.HandleFunc("/", hello)
+	http.ListenAndServe(":80", nil)
 }
